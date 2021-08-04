1885fa7b370469ec967bae78223acd9ecbd13efb
郭和平 <1973821311@qq.com>
2021-08-04 17:19:30 +0800

    设置右键菜单被选中，只显示选中一个菜单

--- DesktopReminder/TrayMenu.cs
+++ DesktopReminder/TrayMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,20 +30,65 @@ namespace DesktopReminder
             Environment.Exit(Environment.ExitCode);    //结束进程
             //this.Close();         //窗体关闭了，但是程序进程以仍在运行                   
         }
-
+        //修改注册表控制程序自动运行
         private void 开机自动运行ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;     //应用程序完整路径
+            //获取可执行文件名称，不包含路径，包含后缀名
+            string strName = Application.ExecutablePath.Substring(Application.ExecutablePath.LastIndexOf("/")+1);
+            //读取注册表指定项
+            //RegistryKey RKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            //if (RKey == null)    //若指定的子项不存在
+            //{
+            //    //以创建的方式打开指定项
+            //    RKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+            //    RKey.SetValue(strName, path);          //设置该子项的新“键值对”
+            //}
+            //else
+            //{
+            //    RKey.DeleteValue(strName, false);         //若指定的子项存在，删除指定的“键名的值”;
+            //}
+            //开机自启动的两种方式
+            // https://www.cnblogs.com/mq0036/p/12117955.html
+            //设置当前菜单项被选中，上一个菜单项取消选中
+            //item是ToolStripItem类型的，需要强制转换为ToolStripMenuItem，否则没有.checked选项
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)cms_tryMenu.Items[1];   //菜单灰度校正的子菜单
+            for (int i = 0; i < menuItem.DropDownItems.Count; i++)
+            {
+                if (menuItem.DropDownItems[i] == (ToolStripMenuItem)sender)
+                    ((ToolStripMenuItem)menuItem.DropDownItems[i]).Checked = true;
+                else
+                    ((ToolStripMenuItem)menuItem.DropDownItems[i]).Checked = false;
 
+            }
         }
 
         private void 取消开机自动运行ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //((ToolStripMenuItem)sender).Checked = true;
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)cms_tryMenu.Items[1];   //菜单灰度校正的子菜单
+            for (int i = 0; i < menuItem.DropDownItems.Count; i++)
+            {
+                if (menuItem.DropDownItems[i] == (ToolStripMenuItem)sender)
+                    ((ToolStripMenuItem)menuItem.DropDownItems[i]).Checked = true;
+                else
+                    ((ToolStripMenuItem)menuItem.DropDownItems[i]).Checked = false;
 
+            }
         }
 
         private void 设置定时关机功能ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)cms_tryMenu.Items[1];   //菜单灰度校正的子菜单
+            for (int i = 0; i < menuItem.DropDownItems.Count; i++)
+            {
+                if (menuItem.DropDownItems[i] == (ToolStripMenuItem)sender)
+                    ((ToolStripMenuItem)menuItem.DropDownItems[i]).Checked = true;
+                else
+                    ((ToolStripMenuItem)menuItem.DropDownItems[i]).Checked = false;
 
+            }
         }
     }
 
